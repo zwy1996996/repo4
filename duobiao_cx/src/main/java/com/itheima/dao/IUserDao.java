809317fa6f19a2b26a1c0a62b16ba70dcb618b9c
zhangwenyan <17865202361@163.com>
2019-04-29 20:50:28 +0800

    delete

--- duobiao_cx/src/main/java/com/itheima/dao/IUserDao.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.dao;
-
-import com.itheima.domain.User;
-
-public interface IUserDao {
-
-    User findById(Integer id);
-}
--- untitled/day1701/src/cn/itheima/domain/User.java
+++
@@ -1,8 +0,0 @@
-package cn.itheima.domain;
-
-public class User {
-    private int id;
-    private String name;
-    private String sex;
-    private String
-}
